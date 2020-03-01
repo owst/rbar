66eac8471127b1169723bcc2ce9bd21845693836
Owen Stephens <owen@owenstephens.co.uk>
2020-03-01 13:17:59 +0000

    Opt-in to all parser-gem options

--- lib/rbar.rb
+++ lib/rbar.rb
@@ -2,6 +2,8 @@ require 'parser/current'
 
 Parser::Builders::Default.emit_lambda = true
 Parser::Builders::Default.emit_procarg0 = true
+Parser::Builders::Default.emit_encoding = true
+Parser::Builders::Default.emit_index = true
 
 require "rbar/version"
 require "rbar/cli"
